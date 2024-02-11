c31ba92f527c2c09cafd8fb8a20d2d0ccc940112
Asjica <siyovush.2000.abdulloev>
2024-02-11 21:49:49 +0500

    Added store method of SubstanceController

--- app/Http/Controllers/Admin/Dictionaries/SubstanceController.php
+++ app/Http/Controllers/Admin/Dictionaries/SubstanceController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Admin\Dictionaries;
 
 use App\Actions\Substance\IndexAction;
+use App\Actions\Substance\StoreAction;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Substance\StoreRequest;
+use App\Http\Resources\SubstanceResource;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
@@ -20,5 +23,12 @@ class SubstanceController extends Controller
     public function create(): Application|View|Factory
     {
         return view('admin.substance.create');
+    }
+
+    public function store(StoreRequest $request, StoreAction $action): SubstanceResource
+    {
+        $substance = $action->execute($request->getParams());
+
+        return new SubstanceResource($substance);
     }
 }
