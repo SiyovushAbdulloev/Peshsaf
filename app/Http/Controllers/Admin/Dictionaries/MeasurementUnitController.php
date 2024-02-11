5c080e9e48593d12ca6425a70ee7aaacd12322a2
Asjica <siyovush.2000.abdulloev>
2024-02-11 15:54:28 +0500

    Added create method of MeasurementController (wip)

--- app/Http/Controllers/Admin/Dictionaries/MeasurementUnitController.php
+++ app/Http/Controllers/Admin/Dictionaries/MeasurementUnitController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers\Admin\Dictionaries;
 
 use App\Actions\MeasurementUnit\IndexAction;
+use App\Actions\MeasurementUnit\StoreAction;
 use App\Http\Controllers\Controller;
+use App\Models\MeasurementUnit;
+use App\Http\Requests\MeasurementUnit\StoreRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Redirector;
 use Illuminate\View\View;
 
 class MeasurementUnitController extends Controller
@@ -13,5 +18,19 @@ class MeasurementUnitController extends Controller
         $units = $action->execute();
 
         return view('admin.dictionaries.measurement-units.index', compact('units'));
+    }
+
+    public function create(): View
+    {
+        $unit = new MeasurementUnit;
+
+        return view('admin.dictionaries.measurement-units.create', compact('unit'));
+    }
+
+    public function store(StoreRequest $request, StoreAction $action): RedirectResponse
+    {
+        $action->execute($request->getParams());
+
+        return redirect(route('dictionaries.measurement-units.index'))->with('success', 'Единица измерения добавлена');
     }
 }
