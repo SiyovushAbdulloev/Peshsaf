c493c9a6e04e2e57a8727020305b24eaf84afe2c
Azim Ablakulov <ablakulovazim@gmail.com>
2024-02-24 02:58:58 +0500

    Добавлен справочник Товары

---
+++ app/Models/Dictionaries/Product.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Dictionaries;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $table = 'dic_products';
+}
---
+++ database/migrations/2024_02_23_215532_create_dic_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dic_products', function (Blueprint $table) {
+            $table->id();
+            $table->string('status');
+            $table->foreignId('category_id')->constrained();
+            $table->foreignId('active_ingredient_id')->constrained();
+            $table->foreignId('measure_id')->constrained();
+            $table->foreignId('country_id')->constrained();
+            $table->string('barcode')->nullable()->unique();
+            $table->date('expiry_date')->nullable();
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dic_products');
+    }
+};
