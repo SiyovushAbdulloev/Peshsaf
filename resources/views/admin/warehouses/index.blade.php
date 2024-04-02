e29dc9d4f49b4dc0dae5fab3a74309dcbcfd126b
Azim Ablakulov <ablakulovazim@gmail.com>
2024-04-02 12:10:40 +0500

    Небольшие исправления

--- resources/views/admin/warehouses/index.blade.php
+++ resources/views/admin/warehouses/index.blade.php
@@ -6,107 +6,133 @@
 
 @section('content')
     <div class="mt-5">
-        <div class="intro-y mt-2 flex">
+        <div class="intro-y my-5 flex gap-4">
             <h2 class="intro-y text-lg font-medium">Склады</h2>
 
-            <a href="{{ route('admin.warehouses.create') }}" class="mb-2 transition duration-200 border
-                inline-flex items-center
-                justify-center py-2
-                px-3 rounded-md ml-auto
-                font-medium cursor-pointer focus:ring-4 focus:ring-primary focus:ring-opacity-20 focus-visible:outline-none dark:focus:ring-slate-700 dark:focus:ring-opacity-50 [&amp;:hover:not(:disabled)]:bg-opacity-90 [&amp;:hover:not(:disabled)]:border-opacity-90 [&amp;:not(button)]:text-center disabled:opacity-70 disabled:cursor-not-allowed bg-primary border-primary text-white dark:border-primary mr-2 shadow-md">
-                <x-base.icon icon="fa-plus" class="mr-2" />
+            <a href="{{ route('admin.warehouses.create') }}" class="transition duration-200 border
+            inline-flex items-center
+            justify-center py-2
+            px-3 rounded-md ml-auto
+            font-medium cursor-pointer focus:ring-4 focus:ring-primary focus:ring-opacity-20 focus-visible:outline-none dark:focus:ring-slate-700 dark:focus:ring-opacity-50 [&amp;:hover:not(:disabled)]:bg-opacity-90 [&amp;:hover:not(:disabled)]:border-opacity-90 [&amp;:not(button)]:text-center disabled:opacity-70 disabled:cursor-not-allowed bg-primary border-primary text-white dark:border-primary mr-2 shadow-md">
+                <x-base.icon icon="fa-plus" class="mr-2"/>
                 Добавить
             </a>
         </div>
 
-        @if($warehouses->count())
-            <table
-                id="receipts-table"
-                data-tw-merge
-                class="w-full text-left mt-5"
-            >
-                <thead data-tw-merge class="">
-                <tr
+        <div class="box mt-5 p-4 overflow-x-auto">
+            @if($warehouses->count())
+                <table
+                    id="receipts-table"
                     data-tw-merge
-                    class="[&amp;:nth-of-type(odd)_td]:bg-slate-100 [&amp;:nth-of-type(odd)_td]:dark:bg-darkmode-300 [&amp;:nth-of-type(odd)_td]:dark:bg-opacity-50"
+                    class="w-full text-left mt-5"
                 >
-                    <th
-                        data-tw-merge
-                        class="font-medium px-5 py-3 border-b-2 dark:border-darkmode-300 whitespace-nowrap"
-                    >
-                        #
-                    </th>
-                    <th
-                        data-tw-merge
-                        class="font-medium px-5 py-3 border-b-2 dark:border-darkmode-300 whitespace-nowrap"
-                    >
-                        Наименование
-                    </th>
-                    <th
-                        data-tw-merge
-                        class="font-medium px-5 py-3 border-b-2 dark:border-darkmode-300 whitespace-nowrap"
-                    >
-                        Адресс
-                    </th>
-                    <th
-                        data-tw-merge
-                        class="font-medium px-5 py-3 border-b-2 dark:border-darkmode-300 whitespace-nowrap"
-                    >
-                        Телефон
-                    </th>
-                    <th
-                        data-tw-merge
-                        class="font-medium px-5 py-3 border-b-2 dark:border-darkmode-300 whitespace-nowrap w-[12%]"
-                    >
-                        &nbsp;
-                    </th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($warehouses as $warehouse)
+                    <thead data-tw-merge class="">
                     <tr
                         data-tw-merge
                         class="[&amp;:nth-of-type(odd)_td]:bg-slate-100 [&amp;:nth-of-type(odd)_td]:dark:bg-darkmode-300 [&amp;:nth-of-type(odd)_td]:dark:bg-opacity-50"
                     >
-                        <td
+                        <th
                             data-tw-merge
-                            class="px-5 py-2 border-b dark:border-darkmode-300"
+                            class="font-medium px-5 py-3 border-b-2 dark:border-darkmode-300 whitespace-nowrap"
                         >
-                            {{ $warehouse->name }}
-                        </td>
-                        <td
+                            #
+                        </th>
+                        <th
                             data-tw-merge
-                            class="px-5 py-2 border-b dark:border-darkmode-300"
+                            class="font-medium px-5 py-3 border-b-2 dark:border-darkmode-300 whitespace-nowrap"
                         >
-                            {{ $warehouse->address }}
-                        </td>
-                        <td
+                            Наименование
+                        </th>
+                        <th
                             data-tw-merge
-                            class="px-5 py-2 border-b dark:border-darkmode-300"
+                            class="font-medium px-5 py-3 border-b-2 dark:border-darkmode-300 whitespace-nowrap"
                         >
-                            {{ $warehouse->phone }}
-                        </td>
-                        <td
+                            Адресс
+                        </th>
+                        <th
                             data-tw-merge
-                            class="px-5 py-2 border-b dark:border-darkmode-300 flex gap-2"
+                            class="font-medium px-5 py-3 border-b-2 dark:border-darkmode-300 whitespace-nowrap"
                         >
-                            <a href="{{ route('admin.warehouses.edit', compact('warehouse')) }}" class="mr-4">
-                                <x-base.icon icon="fa-pen-to-square fa-solid"/>
-                            </a>
-                            <a href="#" class="text-danger">
-                                <x-base.icon icon="fa-trash"></x-base.icon>
-                            </a>
-                        </td>
+                            Телефон
+                        </th>
+                        <th
+                            data-tw-merge
+                            class="font-medium px-5 py-3 border-b-2 dark:border-darkmode-300 whitespace-nowrap w-[12%]"
+                        >
+                            &nbsp;
+                        </th>
                     </tr>
-                @endforeach
-                </tbody>
-            </table>
-        @else
-            <div role="alert"
-                 class="alert relative border rounded-md px-5 py-4 bg-warning border-warning bg-opacity-20 border-opacity-5 text-warning dark:border-warning dark:border-opacity-20 mb-2 flex items-center">
-                <i data-tw-merge data-lucide="alert-circle" class="stroke-1.5 w-5 h-5 mr-2 h-6 w-6 mr-2 h-6 w-6"></i>
-                Нет данных
-            </div>
-        @endif
+                    </thead>
+                    <tbody>
+                    @foreach($warehouses as $warehouse)
+                        <tr
+                            data-tw-merge
+                            class="[&amp;:nth-of-type(odd)_td]:bg-slate-100 [&amp;:nth-of-type(odd)_td]:dark:bg-darkmode-300 [&amp;:nth-of-type(odd)_td]:dark:bg-opacity-50"
+                        >
+                            <td
+                                data-tw-merge
+                                class="px-5 py-2 border-b dark:border-darkmode-300"
+                            >
+                                {{ $warehouse->id }}
+                            </td>
+                            <td
+                                data-tw-merge
+                                class="px-5 py-2 border-b dark:border-darkmode-300"
+                            >
+                                {{ $warehouse->name }}
+                            </td>
+                            <td
+                                data-tw-merge
+                                class="px-5 py-2 border-b dark:border-darkmode-300"
+                            >
+                                {{ $warehouse->address }}
+                            </td>
+                            <td
+                                data-tw-merge
+                                class="px-5 py-2 border-b dark:border-darkmode-300"
+                            >
+                                {{ $warehouse->phone }}
+                            </td>
+                            <td
+                                data-tw-merge
+                                class="px-5 py-2 border-b dark:border-darkmode-300 gap-2 text-right"
+                            >
+                                <x-base.button
+                                    as="a"
+                                    size="sm"
+                                    href="{{ route('admin.warehouses.edit', compact('warehouse')) }}"
+                                    type="button"
+                                    variant="outline-success"
+                                >
+                                    <x-base.icon icon="fa-pen"/>
+                                </x-base.button>
+                                <x-base.button
+                                    size="sm"
+                                    type="button"
+                                    class="delete"
+                                    variant="outline-danger"
+                                    data-route="{{ route('admin.warehouses.destroy', compact('warehouse')) }}"
+                                >
+                                    <x-base.icon icon="fa-trash"/>
+                                </x-base.button>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            @else
+                <div role="alert"
+                     class="alert relative border rounded-md px-5 py-4 bg-warning border-warning bg-opacity-20 border-opacity-5 text-warning dark:border-warning dark:border-opacity-20 mb-2 flex items-center">
+                    <i data-tw-merge data-lucide="alert-circle"
+                       class="stroke-1.5 w-5 h-5 mr-2 h-6 w-6 mr-2 h-6 w-6"></i>
+                    Нет данных
+                </div>
+            @endif
+        </div>
     </div>
+
+    <form id="delete-form" method="POST">
+        @csrf
+        @method('DELETE')
+    </form>
 @endsection
